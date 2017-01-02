0d2ab6885b7fcd56d08eb345f751f3b1006ccba7
Bo Yang <yangbbo@cn.ibm.com>
2017-01-02 18:07:37 +0800

    tabs change

--- C-dashboard/www/js/controllers.js
+++ C-dashboard/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $http, $q) {
 
   //xxxx占比
     // xxxxxxService.getOrderConfCount().then(function(response) {
@@ -40,51 +40,186 @@ angular.module('starter.controllers', [])
     //     // }
     //   });
 
+	var $obj1;
+//	console.log("get data 1111");
+//	var xhr = new XMLHttpRequest();
+//	
+//	xhr.open("GET", "http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report", true);
+//	xhr.onreadystatechange = function($scope, $obj1) {
+//    	if (xhr.readyState == 4) {
+//      		alert(xhr.responseText);
+//      		var data=xhr.responseText
+////    		alert("data=>")
+//    		
+////  			alert(data);
+//      		$scope.obj1 = JSON.parse(data); 
+//      		$obj1 = JSON.parse(data); 
+//      		
+//    		alert($scope.obj1);
+//    		alert($scope.obj1.was);
+//    		return $scope.obj1;
+//    	}
+//	}
+//	xhr.send();
+	var msgdata ="abc";
+	console.log(msgdata);
+	var defer = $q.defer();
+	$http.get('http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report', {
+	    	headers: {"Content-Type": "application/x-www-form-urlencoded",
+	        "Accept": "application/json",
+	        "Access-Control-Allow-Origin": "*",
+	        "Access-Control-Allow-Headers": "Cache-Control, Pragma, Origin, Authorization,   Content-Type, X-Requested-With",
+	        "Access-Control-Allow-Methods": "GET, PUT, POST"} }
+	    ).success(function (data,status,headers,congfig) {
+	    	
+	    	
+	    	defer.resolve(data);
+	    	$obj1 = angular.toJson(data); 
+	    	$msgdata=angular.toJson(data); 
+	    	
+//	      alert("success"+$msgdata);
+//	      alert("success"+data);
+//	      alert("success"+data.was);
+	      
+	      var db2num = parseInt(data.db2);
+	      var wasnum = parseInt(data.was);
+	      var mongoDBnum = parseInt(data.mongoDB);
+	      
+	      var totalnum=db2num+wasnum+mongoDBnum;
+	      
+	      var db2share = (db2num/totalnum)*100;
+	      var wasshare = (wasnum/totalnum)*100;
+	      var mongodbshare = (mongoDBnum/totalnum)*100;
+	      console.log("db2:"+db2share)
+	      console.log("mongodbshare:"+mongodbshare)
+	      console.log("was:"+wasshare)
+	      
+	      
+//	      alert("defer"+ JSON.stringify(defer) );
+	      
+	      ///////////////////////////
+	      //饼图
+	      $scope.chartPieConfig = {
+
+	       chart: {
+	                 plotBackgroundColor: null,
+	                 plotBorderWidth: null,
+	                 plotShadow: false
+	             },
+	             title: {
+	                 text: 'Browser resource usage at a specific environment, 2016'
+	             },
+	             tooltip: {
+	              pointFormat: '{series.name}bb: <b>{point.percentage:2.2f}%</b>'
+	             },
+	             plotOptions: {
+	                 pie: {
+	                     allowPointSelect: true,
+	                     cursor: 'pointer',
+	                     dataLabels: {
+	                         enabled: true,
+	                         color: '#000000',
+	                         connectorColor: '#000000',
+	                         format: '<b>{point.name}cc</b>: {point.percentage:1.1f} %'
+	                     }
+	                 }
+	             },
+	             series: [{
+	                 type: 'pie',
+	                 name: 'Browser share',
+	                 data: [
+	                     ['DB2',   db2share],
+	                     ['WAS',       wasshare],
+	                     {
+	                         name: 'MongoDB',
+	                         y: mongodbshare,
+	                         sliced: true,
+	                         selected: true
+	                     }
+	                 ]
+	             }]
+	      };
+	      /////////////////////////////
+	      
+	      return $msgdata;
+	    }).error(function(data, status, headers, config){	
+	    	defer.reject(data);
+	      alert("登录出错" + data + "  " + status);
+	    });
+	console.log(JSON.stringify(defer.promise));
+	
+	
+	console.log(defer.promise.db2);
+//	alert(obj.db2);
+	
   //饼图
- $scope.chartPieConfig = {
+// $scope.chartPieConfig = {
+//
+//  chart: {
+//            plotBackgroundColor: null,
+//            plotBorderWidth: null,
+//            plotShadow: false
+//        },
+//        title: {
+//            text: 'Browser resource usage at a specific environment, 2016'
+//        },
+//        tooltip: {
+//         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//        },
+//        plotOptions: {
+//            pie: {
+//                allowPointSelect: true,
+//                cursor: 'pointer',
+//                dataLabels: {
+//                    enabled: true,
+//                    color: '#000000',
+//                    connectorColor: '#000000',
+//                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+//                }
+//            }
+//        },
+//        series: [{
+//            type: 'pie',
+//            name: 'Browser share',
+//            data: [
+//                ['Firefox',   45.0],
+//                ['IE',       26.8],
+//                {
+//                    name: 'Chrome',
+//                    y: 12.8,
+//                    sliced: true,
+//                    selected: true
+//                },
+//                ['Safari',    8.5],
+//                ['Opera',     6.2],
+//                ['Others',   0.7]
+//            ]
+//        }]
+// };
 
-  chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Browser resource usage at a specific environment, 2016'
-        },
-        tooltip: {
-         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    color: '#000000',
-                    connectorColor: '#000000',
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Browser share',
-            data: [
-                ['Firefox',   45.0],
-                ['IE',       26.8],
-                {
-                    name: 'Chrome',
-                    y: 12.8,
-                    sliced: true,
-                    selected: true
-                },
-                ['Safari',    8.5],
-                ['Opera',     6.2],
-                ['Others',   0.7]
-            ]
-        }]
- };
+})
 
+
+
+.controller('ChatsCtrl2', function($scope, $resource) {
+		console.log("hello1");
+	    var dataService = $resource('http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report');
+	    $scope.data = dataService.get();
+	    console.log($scope.data);
+	  })
+
+.controller('ChatsCtrl3', function($scope, Chats) {
+	$scope.login = function() {
+		console.log("hello webservice 1111");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report", true);
+		xhr.onreadystatechange = function() {
+	    	if (xhr.readyState == 4) {
+	      		alert(xhr.responseText);
+	    	}
+		}
+		xhr.send();
+	};
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -96,6 +231,18 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+	
+		console.log("hello webservice 1111");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report", true);
+		xhr.onreadystatechange = function() {
+	    	if (xhr.readyState == 4) {
+	      		alert(xhr.responseText);
+	    	}
+		}
+		xhr.send();
+
+	  	  
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
@@ -106,8 +253,50 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, $http, Accounts) {
+	alert("AccountCtrl");
+	  Accounts.all($http).success(function(data) {
+		  	$scope.accounts = data
+		  	alert("ii:"+$scope.accounts);
+			alert("jj:"+JSON.stringify($scope.accounts));
+		      });
+	  
+	  var accounts = [{
+		    id: 0,
+		    name: 'Bo',
+		    lastText: 'You on your way?',
+		    face: 'img/ben.png'
+		  }, {
+		    id: 1,
+		    name: 'Max Lynx',
+		    lastText: 'Hey, it\'s me',
+		    face: 'img/max.png'
+		  }, {
+		    id: 2,
+		    name: 'Adam Bradleyson',
+		    lastText: 'I should buy a boat',
+		    face: 'img/adam.jpg'
+		  }, {
+		    id: 3,
+		    name: 'Perry Governor',
+		    lastText: 'Look at my mukluks!',
+		    face: 'img/perry.png'
+		  }, {
+		    id: 4,
+		    name: 'Mike Harrington',
+		    lastText: 'This is wicked good ice cream.',
+		    face: 'img/mike.png'
+		  }];
+	  
+//	$scope.accounts = Accounts.all($http);
+	  $scope.accounts =  accounts;
+	alert($scope.accounts);
+//	alert(JSON.stringify($scope.accounts));
+	  $scope.remove = function(account) {
+		  Accounts.remove(account);
+	  };
+	  
+//	  Accounts.all($http).success(function(data) {
+//		  	$scope.accounts = data
+//		      });
 });
--- C-dashboard/www/js/services.js
+++ C-dashboard/www/js/services.js
@@ -3,10 +3,12 @@ angular.module('starter.services', [])
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
+	
+	alert("chats");
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'Bo',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
@@ -31,6 +33,19 @@ angular.module('starter.services', [])
     face: 'img/mike.png'
   }];
 
+  console.log("hello here222");
+	
+  console.log("hello webservice 123");
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report", true);
+	xhr.onreadystatechange = function() {
+  	if (xhr.readyState == 4) {
+    		alert(xhr.responseText);
+  	}
+	}
+	xhr.send();
+ 
+	
   return {
     all: function() {
       return chats;
@@ -47,4 +62,124 @@ angular.module('starter.services', [])
       return null;
     }
   };
-});
+})
+
+
+.factory('Accounts', function($http) {
+  // Might use a resource here that returns a JSON array
+	alert("accounts");
+	
+	  return {
+		    all: function($http) {
+		    	
+		    	
+		    	var accounts = $http({
+			         method: 'GET',
+			         url: 'http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report'
+			      }).success(function(data) {
+			    	  alert("account data:"+data);
+			    	  alert(JSON.stringify(data));
+				         // removed your return data; it doesn't do anything, and this success is only added to log the result. if you don't need the log other than for debugging, get rid of this success handler too.   
+				      });
+		       return accounts;
+		    },
+		    remove: function(chat) {
+		    	accounts.splice(accounts.indexOf(chat), 1);
+		    },
+		    get: function(chatId) {
+		      for (var i = 0; i < accounts.length; i++) {
+		        if (accounts[i].id === parseInt(chatId)) {
+		          return accounts[i];
+		        }
+		      }
+		      return null;
+		    }
+		  };
+		  
+//	this.getAccounts  = function() {
+//	       var accounts = $http({
+//		         method: 'GET',
+//		         url: 'http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report'
+//		      }).success(function(data) {
+//		    	  alert("account data:"+data);
+//			         $log.log(data);
+//			         // removed your return data; it doesn't do anything, and this success is only added to log the result. if you don't need the log other than for debugging, get rid of this success handler too.   
+//			      });
+//	       return accounts;
+//	};
+//	return this.getAccounts;
+//	alert("accounts");
+//  // Some fake testing data
+//  var accounts = [{
+//    id: 0,
+//    name: 'Bo',
+//    lastText: 'You on your way?',
+//    face: 'img/ben.png'
+//  }, {
+//    id: 1,
+//    name: 'Max Lynx',
+//    lastText: 'Hey, it\'s me',
+//    face: 'img/max.png'
+//  }, {
+//    id: 2,
+//    name: 'Adam Bradleyson',
+//    lastText: 'I should buy a boat',
+//    face: 'img/adam.jpg'
+//  }, {
+//    id: 3,
+//    name: 'Perry Governor',
+//    lastText: 'Look at my mukluks!',
+//    face: 'img/perry.png'
+//  }, {
+//    id: 4,
+//    name: 'Mike Harrington',
+//    lastText: 'This is wicked good ice cream.',
+//    face: 'img/mike.png'
+//  }];
+//
+//  console.log("hello account");
+//	
+//  console.log("hello webservice 123");
+//	var xhr = new XMLHttpRequest();
+//	xhr.open("GET", "http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/report", true);
+//	xhr.onreadystatechange = function() {
+//  	if (xhr.readyState == 4) {
+//  			var data=xhr.responseText
+//  			var jdata={"provisions":"453","db2":"94","vm":"888","was":"36","title":"Sol-DT Provision Statistic","mongoDB":"31"};
+//    		alert("data=>")
+////    		
+//  			alert(data);
+//    		var obj = JSON.parse(data); 
+////    		alert(obj);
+////    		alert(obj.was);
+////    		alert("jdata=>")
+////    		alert(jdata);
+//    		var obj2 = eval(jdata);
+////    		alert(obj2);
+////    		alert(obj2.db2);
+//  	}
+//	}
+//	xhr.send();
+//	
+//	accounts=xhr.responseText;
+//	alert("==>"+accounts);
+// 
+//	
+//  return {
+//    all: function() {
+//    	alert("response:"+accounts);
+//      return accounts;
+//    },
+//    remove: function(account) {
+//    	accounts.splice(accounts.indexOf(account), 1);
+//    },
+//    get: function(accountId) {
+//      for (var i = 0; i < accounts.length; i++) {
+//        if (accounts[i].id === parseInt(accountId)) {
+//          return accounts[i];
+//        }
+//      }
+//      return null;
+//    }
+//  };
+})

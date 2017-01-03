25bd20e4e7f3bda74356223ef704f679a8d6693b
Bo Yang <yangbbo@cn.ibm.com>
2017-01-03 23:53:58 +0800

    test location

--- C-dashboard/www/js/services.js
+++ C-dashboard/www/js/services.js
@@ -63,7 +63,24 @@ angular.module('starter.services', [])
 .factory('Accounts', function($http) {
   // Might use a resource here that returns a JSON array
 //	alert("accounts");
-	
+	navigator.geolocation.getCurrentPosition( // 该函数有如下三个参数
+	        function(pos){ // 如果成果则执行该回调函数
+	            alert(
+	                '  经度：' + pos.coords.latitude +
+	                '  纬度：' + pos.coords.longitude +
+	                '  高度：' + pos.coords.altitude +
+	                '  精确度(经纬)：' + pos.coords.accuracy +
+	                '  精确度(高度)：' + pos.coords.altitudeAccuracy +
+	                '  速度：' + pos.coords.speed
+	            );
+	        }, function(err){ // 如果失败则执行该回调函数
+	            alert(err.message);
+	        }, { // 附带参数
+	            enableHighAccuracy: false, // 提高精度(耗费资源)
+	            timeout: 3000, // 超过timeout则调用失败的回调函数
+	            maximumAge: 1000 // 获取到的地理信息的有效期，超过有效期则重新获取一次位置信息
+	        }
+	    );
 	  return {
 		    all: function($http) {
 		    	
